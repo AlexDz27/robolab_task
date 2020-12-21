97892a86c815ec0e55ccd9fe763a76baf95036ce
Alex Dzyuba <alexeydzyuba27@gmail.com>
2020-12-21 23:15:59 +0300

    Add teachers and their profiles

--- src/Controller/TeachersController.php
+++ src/Controller/TeachersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Teacher;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +14,56 @@ class TeachersController extends AbstractController
    */
   public function index(): Response
   {
+    $teachers = $this->getDoctrine()
+      ->getRepository(Teacher::class)
+      ->findAll()
+    ;
+
     return $this->render('teachers/index.html.twig', [
-      'controller_name' => 'TeachersController',
+      'teachers' => $teachers,
     ]);
+  }
+
+  /**
+   * @Route("/teacher/{id}", name="teacher_show")
+   */
+  public function show(int $id): Response
+  {
+    /**
+     * @var $teacher Teacher
+     */
+    $teacher = $this->getDoctrine()
+      ->getRepository(Teacher::class)
+      ->find($id)
+    ;
+
+    if (!$teacher) {
+      throw $this->createNotFoundException(
+        'Учителя с id ' . $id . ' не найдено!'
+      );
+    }
+
+    return $this->render('teachers/show.html.twig', [
+        'teacher' => $teacher
+     ]);
+  }
+
+  /**
+   * @Route("/teachers/test/create", name="teachers_test_create")
+   */
+  public function createTestTeacher(): Response
+  {
+    $entityManager = $this->getDoctrine()->getManager();
+
+    $teacher = new Teacher();
+    $teacher->setName('Валентина');
+    $teacher->setMiddleName('Ивановна');
+    $teacher->setSurname('Чайкова');
+
+    $entityManager->persist($teacher);
+
+    $entityManager->flush();
+
+    return new Response('Saved new teacher with id: ' . $teacher->getId());
   }
 }
---
+++ src/Entity/Teacher.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TeacherRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TeacherRepository::class)
+ */
+class Teacher
+{
+  /**
+   * @ORM\Id
+   * @ORM\GeneratedValue
+   * @ORM\Column(type="integer")
+   */
+  private $id;
+
+  /**
+   * @ORM\Column(type="string", length=255)
+   */
+  private $name;
+
+  /**
+   * @ORM\Column(type="string", length=255)
+   */
+  private $surname;
+
+  /**
+   * @ORM\Column(type="string", length=255, nullable=true)
+   */
+  private $middleName;
+
+  /**
+   * @ORM\Column(type="string", length=500, nullable=true)
+   */
+  private $imgPath;
+
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
+
+  public function getName(): ?string
+  {
+    return $this->name;
+  }
+
+  public function setName(string $name): self
+  {
+    $this->name = $name;
+
+    return $this;
+  }
+
+  public function getSurname(): ?string
+  {
+    return $this->surname;
+  }
+
+  public function setSurname(string $surname): self
+  {
+    $this->surname = $surname;
+
+    return $this;
+  }
+
+  public function getMiddleName(): ?string
+  {
+    return $this->middleName;
+  }
+
+  public function setMiddleName(?string $middleName): self
+  {
+    $this->middleName = $middleName;
+
+    return $this;
+  }
+
+  public function getFullName(): string
+  {
+    return "{$this->getSurname()} {$this->getName()} {$this->getMiddleName()}";
+  }
+
+  public function getImgPath(): ?string
+  {
+      return $this->imgPath;
+  }
+
+  public function setImgPath(?string $imgPath): self
+  {
+      $this->imgPath = $imgPath;
+
+      return $this;
+  }
+}
